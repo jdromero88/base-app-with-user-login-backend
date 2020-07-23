f2ec3d1c40d85c51de6c39a3a4a59961f435c8ec
jdromero88 <jdromerov88@gmail.com>
2020-07-22 20:30:56 -0400

    Active storage working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    # byebug
+    @user = User.create(email: params[:email], password: params[:password])
+    render json: @user
   end
 
   def show
@@ -13,15 +15,20 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email])
     @avatar = rails_blob_path(@user.avatar)
     if @user.authenticate(params[:password])
-      puts('Password matched')
+      # puts('Password matched')
       render json: {user: @user, avatar: @avatar}
     else
-      puts('wrong password')
+      # puts('wrong password')
       render json: {message: 'This user is not authenticated.'}
     end
   end
 
   def update
-
+    # byebug
+    @user = User.find(params[:id])
+    @user.update(avatar: params[:avatar])
+    @avatar_url = rails_blob_path(@user.avatar)
+    # byebug
+    render json: { user: @user, avatar_url: @avatar_url}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :products
   resources :users
   post '/login', to: 'users#show'
+  post '/rails/active_storage/direct_uploads', to: 'direct_uploads#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
