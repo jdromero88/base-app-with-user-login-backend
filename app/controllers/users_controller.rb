af8c6fd7b905549dadbaa5ddbb23f19e22d8eb80
jdromero88 <jdromerov88@gmail.com>
2020-07-29 13:51:37 -0400

    Added serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    render json: @users
+    render json: UserSerializer.new(@users).to_serialized_json
   end
 
   def create
     # byebug
-    @user = User.create(email: params[:email], password: params[:password])
-    render json: @user
+    @user = User.create!(user_strong_params)
+    if @user
+      render json: UserSerializer.new(@user).to_serialized_json
+    else
+      render json: {
+        error: true,
+        message: 'Email already in use. Try a different one.'
+      }, status: :not_acceptable
+    end
   end
 
   def show
@@ -31,4 +38,16 @@ class UsersController < ApplicationController
     # byebug
     render json: { user: @user, avatar_url: @avatar_url}
   end
+
+  private
+    def user_strong_params
+      params.permit(
+        :user,
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :bio
+      )
+    end
 end
