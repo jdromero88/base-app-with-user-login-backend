a6364a3b478acb9fb4244e1d5efb125abdd841a9
jdromero88 <jdromerov88@gmail.com>
2020-08-24 23:48:21 -0400

    Added product serialization

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,53 @@ class ProductsController < ApplicationController
     @products = Products.all
     render json: @products
   end
+
+  def create
+    @product = Product!(product_strong_params)
+    if @product
+      render json: ProductSerializer.new(@product).to_serialized_json
+    else
+      render json: {
+        error:true,
+        message: 'Product already exist.'
+      }, status: :not_acceptable
+    end
+  end
+
+  def show
+    @product = Product.find_by(name: params[:name])
+    if @product
+      render json: ProductSerializer.new(@product).to_serialized_json
+    else
+      render json: {
+        error: true,
+        message: 'Product not found.'
+      }
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product
+      @product.update!(product_strong_params)
+      render json: ProductSerializer.new(@product).to_serialized_json
+    else
+      render json: {
+        error: true,
+        message: 'Product not found.'
+      }
+    end
+
+  end
+
+  private
+    def product_strong_params
+      params.permit(
+        :name,
+        :description,
+        :image,
+        :price,
+        :active
+      )
+    end
 end
---
+++ app/services/product_serializer.rb
@@ -0,0 +1,12 @@
+# Class for serializing the product data. Doing this will help scalating
+# the app and removing logic from the controller making it easier to read code.
+class UserSerializer
+  def initialize(product_object)
+    @product = product_object
+  end
+
+  # serializer method to exclude data we dont need in our front end.
+  def to_serialized_json
+    @product.to_json(:except => [:created_at, :updated_at])
+  end
+end
